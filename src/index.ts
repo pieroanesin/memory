821d89d6e7a2e053a093ded0ccc4b43a34b9f876
Piero Anesin <pieroanesin@gmail.com>
2024-10-27 19:42:29 +0100

    feat: show value of card when clicked

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const board = document.getElementById('board')!
 
-  const card = document.createElement('div')
-  card.classList.add('card')
+  const cardValues: string[] = ['cat', 'dog', 'mouse']
 
-  board.appendChild(card)
+  cardValues.forEach(value => {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.dataset.value = value
+    card.addEventListener('click', () => {
+      flipCard(card)
+    })
+
+    board.appendChild(card)
+  })
+
+  function flipCard(card: HTMLElement) {
+    card.textContent = card.dataset.value || ''
+  }
 })
